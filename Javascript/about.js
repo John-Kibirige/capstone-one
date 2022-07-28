ec7e9fc8b2c9a5ee1defef5907cf78283cbab5d5
John Kibirige <kibirigejohn005@gmail.com>
2022-07-28 14:23:13 +0300

    Fix errors as a result of linters

--- Javascript/about.js
+++ Javascript/about.js
@@ -2,8 +2,17 @@
 const humberger = document.querySelector('.header-humberger-menu');
 const headerContainer = document.querySelector('.header-container');
 
+// Add event listener to the humberger menu
 humberger.addEventListener('click', () => {
-  console.log('btn works well');
   humberger.classList.toggle('active');
   headerContainer.classList.toggle('active');
+});
+
+// Monitor screen onchange
+window.addEventListener('resize', () => {
+  const windowSize = window.innerWidth;
+  if (windowSize >= 768) {
+    humberger.classList.remove('active');
+    headerContainer.classList.remove('active');
+  }
 });
